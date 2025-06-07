95f4c07f3d4c33575f4a89c7503ea3a98a99db62
Anurag Basurui <anuragbasuri2502@gmail.com>
2025-06-07 10:55:06 +0530

    Fix: Update import paths in user controller
    Updates import paths in the user controller to use relative paths. This resolves issues with module resolution and ensures the controller can correctly access necessary models, utils, and services.

--- Backend/src/controllers/users/user.controller.js
+++ Backend/src/controllers/users/user.controller.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
-import { User } from "../models/user.models.js";
-import { BloodDonation } from "../models/blood.models.js";
-import { Center } from "../models/center.models.js";
-import { DonationAppointment } from "../models/appointment.models.js";
+import { User } from "../../models/user.models.js";
+import { BloodDonation } from "../../models/blood.models.js";
+import { Center } from "../../models/center.models.js";
+import { DonationAppointment } from "../../models/appointment.models.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 import { ApiError } from "../../utils/ApiError.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
 import { uploadFile, deleteFile } from "../../utils/fileUpload.js";
-import { sendMail } from "../services/mail.service.js";
+import { sendMail } from "../../services/mail.service.js";
 
 // Authentication Controllers
 const registerUser = asyncHandler(async (req, res) => {
